fda33ce8e160f04ff931923231e026ad5796e1ec
subeg7 <subeg7@gmail.com>
2020-12-04 10:30:13 +0545

    firebase integrated for multiple flavors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
+import 'package:flavor_flutter/app_config.dart';
 import 'package:flavor_flutter/ui/pages/my_app.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  final AppConfig appConfig = AppConfig(
+    appEnvironment: AppEnvironment.DEV,
+    appName: "FlavoredApp Production",
+    description: "This is a Production version of Falvored App",
+    baseUrl: 'https://development-flavored-flutter.com/api',
+    themeData: ThemeData(
+      primaryColor: Colors.white,
+      disabledColor: Colors.red,
+    ),
+    child: MyApp(),
+  );
+  runApp(appConfig);
+}
